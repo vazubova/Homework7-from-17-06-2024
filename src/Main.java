b22e5d446363337f989da337b4dea023fd661b21
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-23 12:39:04 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(.);
+
+        int first = 0;
+        int ready = 15000;
+        int m = 1;
+
+        while ( first <= 2459000 ) {
+
+            first = first + ready;
+            System.out.println("Месяц " + m + ", сумма накоплений равна " + first + " рублей");
+            m++;
+        }
+
+
     }
+
+
 }
